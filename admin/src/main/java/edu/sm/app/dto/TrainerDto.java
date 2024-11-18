03ff41de9b568a0c69249693726a47baa6101e3e
kwaksss <kis2690@naver.com>
2024-11-18 19:02:06 +0900

    11.18 admin login,websocket일부 완성

---
+++ admin/src/main/java/edu/sm/app/dto/TrainerDto.java
@@ -0,0 +1,23 @@
+package edu.sm.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrainerDto {
+    private String trainerId;
+    private String trainerName;
+    private String trainerAge;
+    private String trainerGender;
+    private String trainerPhone;
+    private String trainerAddress;
+    private String trainerPwd;
+    private String placeNo;
+    private RoleDto role;
+//    private int arole;
+}
--- admin/src/main/java/edu/sm/controller/MainController.java
+++ admin/src/main/java/edu/sm/controller/MainController.java
@@ -1,34 +1,87 @@
 package edu.sm.controller;
 
+import edu.sm.app.dto.TrainerDto;
+import edu.sm.app.service.TrainerService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
+@RequiredArgsConstructor
 public class MainController {
 
+    private final TrainerService trainerService;
+
+    @Value("${app.url.server-url}")
+    String serverUrl;
+
     @RequestMapping("/")
-    public String main(Model model) {
+    public String main(Model model){
+        model.addAttribute("charturl",serverUrl);
+        model.addAttribute("serverurl",serverUrl);
+
         return "index";
     }
-
     @RequestMapping("/websocket")
-    public String websocket(Model model) {
+    public String websocket(Model model){
+        model.addAttribute("serverurl",serverUrl);//얘가 중요한친구임. 여기에 127.0.0...넣으면 수시로 바꿀 수 없으니가 yml에 다로 빼놓음.
+        model.addAttribute("top","top");
+        model.addAttribute("left","left");
+        model.addAttribute("center","websocket");
         return "index";
     }
 
     @RequestMapping("/chat")
-    public String chat(Model model) {
+    public String chat(Model model){
+        model.addAttribute("serverurl",serverUrl);
+        model.addAttribute("center","chat");
         return "index";
     }
 
-//    @RequestMapping("/logout")
-//    public String logout(HttpSession httpSession) {
-//        return "index";
-//    }
-//
-//    @RequestMapping("/loginimpl")
-//    public String loginimpl(@RequestParam("id") String id, @RequestParam("pwd") String pwd, HttpSession httpSession) {
-//        return "redirect:/";
-//    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpSession httpSession){
+        if(httpSession != null){
+            httpSession.invalidate();
+        }
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model , HttpSession httpSession){
+        if(httpSession != null){
+            httpSession.invalidate();
+        }
+        model.addAttribute("top","top");
+        model.addAttribute("left","left");
+        model.addAttribute("center","login");
+        return "index";
+    }
+
+
+    @RequestMapping("/loginimpl")
+    public String loginimpl(
+            @RequestParam("id") String id,
+            @RequestParam("pwd") String pwd,
+            HttpSession Session) throws Exception {
+        TrainerDto trainerDto = null;
+
+        log.info("여기까지옴");
+
+
+            trainerDto = trainerService.get(id);
+
+
+        Session.setAttribute("loginid",trainerDto);
+        log.info("loginid까지 됨.");
+
+        return "redirect:/";
+    }
+
 }
