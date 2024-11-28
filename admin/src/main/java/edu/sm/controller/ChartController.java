2065a302651bc01c6316b224b55145e46b4ce43c
kwaksss <kis2690@naver.com>
2024-11-28 17:54:24 +0900

    11.28ã 성별차트 완성

---
+++ admin/src/main/java/edu/sm/controller/ChartController.java
@@ -0,0 +1,51 @@
+
+package edu.sm.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.sm.app.service.CustService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/chart")
+@Slf4j
+public class ChartController {
+
+    @Autowired
+    private CustService custService;
+
+
+//    @RequestMapping("")
+//    public String qr(Model model) {//custcheck 화면을 보여준다.
+//
+//
+//        model.addAttribute("center", "qr/" + "center"); // 회원가입 페이지 제목 추가
+//
+//        return "index";
+//    }
+
+    @GetMapping("/gender")
+    public String getGenderChartData(Model model) throws JsonProcessingException {
+        Map<String, Integer> genderCounts = custService.getGenderCounts();
+
+        // genderCounts 값 확인을 위한 로그 추가
+        System.out.println("genderCounts: " + genderCounts);
+
+        model.addAttribute("genderCounts", new ObjectMapper().writeValueAsString(genderCounts));
+        model.addAttribute("center", "chart/gender");
+        model.addAttribute("top", "chart/top");
+        model.addAttribute("left", "chart/left");
+
+        return "index";
+    }
+
+}
