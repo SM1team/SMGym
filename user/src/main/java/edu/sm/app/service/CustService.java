613581ce461c53be8f441a8e0509e2e85a637c21
kwaksss <kis2690@naver.com>
2024-12-10 14:08:36 +0900

    1210 ãPT일지 & 나의 월별 출석chart완성

--- user/src/main/java/edu/sm/app/service/CustService.java
+++ user/src/main/java/edu/sm/app/service/CustService.java
@@ -9,6 +9,7 @@
     import org.springframework.stereotype.Service;
 
     import java.util.List;
+    import java.util.Map;
 
     @Service
     @RequiredArgsConstructor
@@ -62,5 +63,7 @@
         public boolean hasPurchasedPT(String custId) {
             return custRepository.hasPurchasedPT(custId) > 0;
         }
+
+
 
     }
